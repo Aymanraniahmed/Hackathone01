db1804cf2b9f10e91d825347b27cfb1da38a74fe
Aymanraniahmed <164085877+Aymanraniahmed@users.noreply.github.com>
2024-12-18 00:30:39 +0500

    Frist Commit

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, // Skip ESLint during the build
+  },
+};
+
+export default nextConfig;
+
+
